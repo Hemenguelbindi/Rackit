d062eea1efd69b24bbdea79c2e45110f3f54ad16
Karpov Victor <nilafe4@gmail.com>
2025-06-04 15:15:19 +0500

    commit train provaider

--- src/providers/traits.rs
+++ src/providers/traits.rs
@@ -13,4 +13,21 @@ pub trait Provider: Debug + Send + Sync {
     /// Тип идентификатора машины
     type MashineId: Debug + Clone + Serialize + for<'de> Deserialize<'de> + Send + Sync + 'static;
 
+    type MachineInfo: Debug + Clone + Serialize + for<'de> Deserialize<'de> + Send + Sync + 'static;
+
+    fn provider_name(&self) -> &str;
+
+
+    fn is_available(&self) -> bool;
+
+
+    fn list_machines(&self) -> ProviderResult<Vec<Self::MachineId>>;
+
+    fn get_machine_info(&self, machine_id: &Self::MachineId) -> ProviderResult<Self::MachineInfo>;
+
+    fn create_machine(&self, machine_id: &Self::MachineId, config: &Self::MachineConfig) -> ProviderResult<()>{
+        Err("Machine creation is not supported by this provider".into())
+    };
+
+    
 }
